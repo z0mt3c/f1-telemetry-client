b1065876ddd9f957217060778d710b8d1ce51222
Timo Behrmann <timo.behrmann@bdess.com>
2022-07-21 15:18:04 +0200

    f1 22 tests - event data

---
+++ src/mocks/2022/PacketEventData.ts
@@ -0,0 +1,28 @@
+export const PACKET_EVENT_DATA_BUFFER_2022 = [
+  230, 7,   1,   5,   1,   3,  173, 134, 0,   14,  158, 110, 34, 221,
+  198, 4,   150, 66,  211, 13, 0,   0,   19,  255, 83,  80,  84, 80,
+  10,  114, 190, 147, 67,  1,  1,   10,  114, 190, 147, 67
+];
+
+export const PACKET_EVENT_DATA_PARSED_2022 = {
+  m_eventDetails: {
+    fastestSpeedInSession: 295.48785400390625,
+    fastestVehicleIdxInSession: 10,
+    isDriverFastestInSession: 1,
+    isOverallFastestInSession: 1,
+    speed: 295.48785400390625,
+    vehicleIdx: 10,
+  },
+  m_eventStringCode: 'SPTP',
+  m_header: {
+    m_frameIdentifier: 3539,
+    m_gameMajorVersion: 1,
+    m_gameMinorVersion: 5,
+    m_packetFormat: 2022,
+    m_packetId: 3,
+    m_packetVersion: 1,
+    m_playerCarIndex: 19,
+    m_secondaryPlayerCarIndex: 255,
+    m_sessionTime: 75.00932312011719,
+  }
+};
